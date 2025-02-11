37b268e9ed413b9c27d810083af10a0d501e6895
TheRahulS <104826142+TheRahulS@users.noreply.github.com>
2025-02-11 16:17:23 +0530

    server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = 5000;
 const db = require('./config/db_Setting');
 const userRoute = require('./routes/userroutes');
 const adminroute=require('./routes/adminroute')
+const route = require('./routes/route');
 
 const cors = require('cors');
 app.use(express.json());
@@ -11,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 
-
+app.use('/api',route);
 
 app.use('/api/v1', userRoute); // Define API routes
 app.use('/v2',adminroute)
